40071d7de3d195a0deb5bb42cacd60e6354c3e0e
Adam Klinkosz <spyro@o2.pl>
2018-06-13 19:25:51 +0200

    Tests are now using Holiday API mock.

--- src/test/groovy/cleankod/bs/holiday/BaseMvcSpec.groovy
+++ src/test/groovy/cleankod/bs/holiday/BaseMvcSpec.groovy
@@ -1,6 +1,8 @@
 package cleankod.bs.holiday
 
+import cleankod.bs.holiday.client.HolidayClientMockFactory
 import com.fasterxml.jackson.databind.ObjectMapper
+import net.jadler.Jadler
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.context.annotation.Bean
@@ -31,7 +33,7 @@ class BaseMvcSpec extends Specification {
     }
 
     protected MockHttpServletResponse get(String uri, Map<String, List<String>> params) {
-        def entries = params.findAll {key, value -> value != null && !value.empty}
+        def entries = params.findAll { key, value -> value != null && !value.empty }
         MultiValueMap paramsMultiValueMap = new LinkedMultiValueMap(entries)
         mvc.perform(MockMvcRequestBuilders.get(uri).params(paramsMultiValueMap)).andReturn().response
     }
@@ -43,6 +45,11 @@ class BaseMvcSpec extends Specification {
     static class TestContextConfiguration {
         public static final Instant FIXED_NOW = Instant.parse("2018-06-11T12:48:55Z")
         public static final ZoneId FIXED_ZONE = ZoneId.of("UTC")
+
+        @Bean
+        Jadler holidayApiServerMock() {
+            return HolidayClientMockFactory.getInstance()
+        }
 
         @Bean
         @Profile("test")
---
+++ src/test/groovy/cleankod/bs/holiday/client/HolidayClientMockFactory.groovy
@@ -0,0 +1,35 @@
+package cleankod.bs.holiday.client
+
+import net.jadler.Jadler
+import net.jadler.Request
+import net.jadler.stubbing.StubResponse
+import net.jadler.stubbing.server.jdk.JdkStubHttpServer
+
+import java.nio.charset.Charset
+
+class HolidayClientMockFactory {
+
+    static Jadler getInstance() {
+        Jadler.initJadlerUsing(new JdkStubHttpServer(9090))
+
+        def holidaysResponder = { Request request ->
+            def mockResponsesPath = "/holiday-api-responses${request.getURI().path}"
+            def parameters = request.getParameters()
+            def year = parameters.getValue("year")
+            def month = parameters.getValue("month").padLeft(2, '0')
+            def day = parameters.getValue("day").padLeft(2, '0')
+            def country = parameters.getValue("country")
+            def filename = "${mockResponsesPath}/${year}-${month}-${day}_${country}.json"
+            def requestedResource = getClass().getResource(filename)
+            def actualResource = Optional.ofNullable(requestedResource)
+                    .orElseGet({ getClass().getResource("${mockResponsesPath}/empty.json") })
+            return StubResponse.builder()
+                    .body(actualResource.text, Charset.forName("UTF-8"))
+                    .build()
+        }
+        return Jadler.onRequest()
+                .havingMethodEqualTo("GET")
+                .respondUsing(holidaysResponder)
+
+    }
+}
