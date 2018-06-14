9885f4e429585833a1edeacf92368e5ba246e2fc
Adam Klinkosz <spyro@o2.pl>
2018-06-14 16:46:45 +0200

    Holiday domain class now implements serializable in order for the Hazelcast to work properly with it.

--- src/main/java/cleankod/bs/holiday/core/domain/Holiday.java
+++ src/main/java/cleankod/bs/holiday/core/domain/Holiday.java
@@ -1,11 +1,12 @@
 package cleankod.bs.holiday.core.domain;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 
 import lombok.Data;
 
 @Data
-public class Holiday {
+public class Holiday implements Serializable {
     private final String name;
     private final String country;
     private final LocalDate date;
