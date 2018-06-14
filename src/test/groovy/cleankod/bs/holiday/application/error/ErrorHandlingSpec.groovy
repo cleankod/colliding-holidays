3452f4663a18a10c34f29e726318f58427abcb25
Adam Klinkosz <spyro@o2.pl>
2018-06-14 17:52:24 +0200

    Covered another error handling case.

--- src/test/groovy/cleankod/bs/holiday/application/error/ErrorHandlingSpec.groovy
+++ src/test/groovy/cleankod/bs/holiday/application/error/ErrorHandlingSpec.groovy
@@ -21,4 +21,22 @@ class ErrorHandlingSpec extends BaseMvcSpec {
         givenExceptionClassName          || actualStatus
         IllegalStateException.class.name || 500
     }
+
+    def "should handle client exception"() {
+        given:
+        def message = "some message"
+
+        when:
+        def response = get("/throw-client-exception", [message: [message], status: [status as String]])
+
+        then:
+        response.status == status
+        with(getResponseAs(response, ErrorResponse)) {
+            it.id.length() == 16
+            it.globalErrors.get(0).message == message
+        }
+
+        where:
+        status << [401, 402, 403, 429, 500]
+    }
 }
--- src/test/groovy/cleankod/bs/holiday/application/error/ErrorThrowingFakeRestController.groovy
+++ src/test/groovy/cleankod/bs/holiday/application/error/ErrorThrowingFakeRestController.groovy
@@ -1,5 +1,6 @@
 package cleankod.bs.holiday.application.error
 
+import cleankod.bs.holiday.client.exception.HolidayClientException
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
@@ -13,5 +14,13 @@ class ErrorThrowingFakeRestController {
             @RequestParam("message") String message
     ) throws Exception {
         throw (Exception) Class.forName(exceptionClass).getConstructor(String.class).newInstance(message)
+    }
+
+    @GetMapping(path = "/throw-client-exception")
+    String throwException(
+            @RequestParam("status") Integer status,
+            @RequestParam("message") String message
+    ) throws Exception {
+        throw new HolidayClientException(new cleankod.bs.holiday.client.domain.ErrorResponse(status, message))
     }
 }
