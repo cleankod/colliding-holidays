63b9be96affb4a815bd9802b0458cd95979df36b
Adam Klinkosz <spyro@o2.pl>
2018-06-15 11:51:54 +0200

    More functional core colliding holiday fetching logic.

--- src/main/java/cleankod/bs/holiday/core/CollidingHolidayService.java
+++ src/main/java/cleankod/bs/holiday/core/CollidingHolidayService.java
@@ -1,9 +1,11 @@
 package cleankod.bs.holiday.core;
 
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import cleankod.bs.holiday.core.domain.GetCollidingHolidaysRequest;
 import cleankod.bs.holiday.core.domain.Holiday;
@@ -15,19 +17,18 @@ public class CollidingHolidayService {
     private final HolidayPerCountryFetcher holidayPerCountryFetcher;
 
     List<Holiday> getHolidaysOnTheSameDayInAllCountries(GetCollidingHolidaysRequest getCollidingHolidaysRequest) {
-        Map<String, List<Holiday>> holidays;
-        var actualDate = getCollidingHolidaysRequest.getDate();
-        do {
-            holidays = holidayPerCountryFetcher.fetchHolidays(getCollidingHolidaysRequest, actualDate);
-            actualDate = actualDate.plusDays(1L);
-        }
-        while (holidays.size() < getCollidingHolidaysRequest.getCountries().size());
+        return Stream.iterate(getCollidingHolidaysRequest.getDate(), date -> date.plusDays(1L))
+                .map(date -> holidayPerCountryFetcher.fetchHolidays(getCollidingHolidaysRequest, date))
+                .filter(holidays -> holidays.size() >= getCollidingHolidaysRequest.getCountries().size())
+                .findFirst()
+                .map(this::getFlatHolidaysList)
+                .orElse(Collections.emptyList());
+    }
 
+    private List<Holiday> getFlatHolidaysList(Map<String, List<Holiday>> holidays) {
         return holidays.entrySet().stream()
                 .map(Map.Entry::getValue)
                 .flatMap(Collection::stream)
                 .collect(Collectors.toList());
     }
-
-
 }
